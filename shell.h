8bb733e6a7ef7d9e83f7ea8b976b501f90f33c2c
jeron <jeronovalerie1@gmail.com>
2022-05-13 13:33:34 +0300

    shell.h

--- shell.h
+++ shell.h
@@ -125,7 +125,7 @@ void free_list(list_t *head);
 void help_all(void);
 void help_alias(void);
 void help_cd(void);
-void help(void);
+void help_exit(void);
 void help_help(void);
 void help_env(void);
 void help_setenv(void);
